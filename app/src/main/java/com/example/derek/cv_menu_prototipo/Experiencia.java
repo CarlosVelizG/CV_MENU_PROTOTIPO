a89dfeebcaf5cb9abc846bc76fc67cb87537448a
CarlosVelizG <carfullmaxx@gmail.com>
2018-11-04 17:33:55 -0500

    Prototipo CV primer adelanto de la aplicacion

---
+++ app/src/main/java/com/example/derek/cv_menu_prototipo/Experiencia.java
@@ -0,0 +1,16 @@
+package com.example.derek.cv_menu_prototipo;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Experiencia extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_experiencia, container, false);
+    }
+}
